c1fa3e99ff5dfc820473bfa8cb927f6b747c0bb6
JV-Amorim <joaovitoramorimx@gmail.com>
2022-04-30 16:58:40 -0300

    refactor: some functions moved to new files

---
+++ src/input_output.py
@@ -0,0 +1,28 @@
+import sys
+import cv2
+
+
+INPUT_DIR = 'input/'
+OUTPUT_DIR = 'output/'
+
+
+def get_input_img_name_from_arguments():
+  arguments = sys.argv[1:]
+
+  if len(arguments) != 1:
+    raise ValueError('Incorrect number of arguments.')
+
+  input_img_name = arguments[0]
+
+  return input_img_name
+
+def read_input_img(input_img_name):  
+  input_img = cv2.imread(INPUT_DIR + input_img_name)
+
+  if input_img is None:
+    raise ValueError('Input image file not found.')
+
+  return input_img
+
+def save_output_img(output_img, output_img_name):
+  cv2.imwrite(OUTPUT_DIR + output_img_name, output_img)
--- src/main.py
+++ src/main.py
@@ -1,48 +1,17 @@
-import sys
-
-import cv2
-
 from gaussian import GAUSSIAN_KERNEL_5
+from input_output import get_input_img_name_from_arguments, read_input_img, save_output_img
 from kernel import apply_kernel_to_matrix
-
-INPUT_DIR = 'input/'
-OUTPUT_DIR = 'output/'
+from utils import ImageUtils
 
 
 def main():
   input_img_name = get_input_img_name_from_arguments()
   bgr_input_img = read_input_img(input_img_name)
   
-  result_img = convert_bgr_img_to_grayscale(bgr_input_img)
+  result_img = ImageUtils.convert_bgr_img_to_grayscale(bgr_input_img)
   result_img = apply_kernel_to_matrix(GAUSSIAN_KERNEL_5, result_img)
 
   save_output_img(result_img, input_img_name)
-
-
-def get_input_img_name_from_arguments():
-  arguments = sys.argv[1:]
-
-  if len(arguments) != 1:
-    raise ValueError('Incorrect number of arguments.')
-
-  input_img_name = arguments[0]
-
-  return input_img_name
-
-def read_input_img(input_img_name):  
-  input_img = cv2.imread(INPUT_DIR + input_img_name)
-
-  if input_img is None:
-    raise ValueError('Input image file not found.')
-
-  return input_img
-
-def save_output_img(output_img, output_img_name):
-  cv2.imwrite(OUTPUT_DIR + output_img_name, output_img)
-
-
-def convert_bgr_img_to_grayscale(bgr_image):
-  return cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
 
 
 if __name__ == '__main__':
