118a6eb8bbcdf8287cf51f1e1e00df878f22414f
JV-Amorim <joaovitoramorimx@gmail.com>
2022-04-30 17:43:50 -0300

    feat: CannyEdgeDetector class created

--- src/canny.py
+++ src/canny.py
@@ -2,6 +2,40 @@ from consts import GAUSSIAN_KERNEL_5
 from kernel import apply_kernel_to_matrix
 
 
-def apply_canny_algorithm_to_image(img):
-  smoothed_img = apply_kernel_to_matrix(GAUSSIAN_KERNEL_5, img)
-  return smoothed_img
+def apply_canny_edge_detector_to_img(img):
+  canny_edge_detector = CannyEdgeDetector()
+  canny_edge_detector.set_input_img(img)
+  canny_edge_detector.apply_edge_detector()
+  return canny_edge_detector.get_result_img()
+
+
+class CannyEdgeDetector:
+
+  def __init__(self):
+    self.img = None
+    self.result_img = None
+
+
+  def set_input_img(self, img):
+    if len(img.shape) != 2:
+      raise ValueError('The input image must have only two dimensions!')
+
+    self.img = img
+    self.img_height, self.img_width = img.shape
+
+
+  def apply_edge_detector(self):
+    if self.img is None:
+      raise ValueError('The input image must be setted before calling this method.')
+
+    self.__smooth_the_img()
+
+  def __smooth_the_img(self):
+    self.result_img = apply_kernel_to_matrix(GAUSSIAN_KERNEL_5, self.img)
+
+
+  def get_result_img(self):
+    if self.result_img is None:
+      raise ValueError('No result image have been found. Call the apply_edge_detector method before.')
+
+    return self.result_img
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from canny import apply_canny_algorithm_to_image
+from canny import apply_canny_edge_detector_to_img
 from input_output import get_input_img_name_from_arguments, read_input_img, save_output_img
 from utils import ImageUtils
 
@@ -8,7 +8,7 @@ def main():
   bgr_input_img = read_input_img(input_img_name)
 
   result_img = ImageUtils.convert_bgr_img_to_grayscale(bgr_input_img)
-  result_img = apply_canny_algorithm_to_image(result_img)
+  result_img = apply_canny_edge_detector_to_img(result_img)
 
   save_output_img(result_img, input_img_name)
 
