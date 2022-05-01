c72debfbaf3d0abdfc45dc29058d98119fd560bb
JV-Amorim <joaovitoramorimx@gmail.com>
2022-05-01 00:00:25 -0300

    feat: non-maximum suppression implemented

--- src/canny.py
+++ src/canny.py
@@ -8,14 +8,14 @@ def apply_canny_edge_detector_to_img(img):
   canny_edge_detector = CannyEdgeDetector()
   canny_edge_detector.set_input_img(img)
   canny_edge_detector.apply_edge_detector()
-  return canny_edge_detector.get_result_img()
+  return canny_edge_detector.get_result_imgs()
 
 
 class CannyEdgeDetector:
 
   def __init__(self):
     self.img = None
-    self.result_img = None
+    self.applied = False
 
 
   def set_input_img(self, img):
@@ -30,21 +30,23 @@ class CannyEdgeDetector:
     if self.img is None:
       raise ValueError('The input image must be setted before calling this method.')
 
-    self.result_img = np.zeros(self.img.shape)
+    self.applied = True
 
     self.__smooth_with_gaussian_kernel()
     self.__calculate_gradient_with_sobel_kernels()
     self.__calculate_gradient_magnitude()
     self.__calculate_gradient_direction()
+    self.__perform_nonmaximum_suppression()
+    self.thresholded_img = np.zeros(self.img.shape)
 
 
   def __smooth_with_gaussian_kernel(self):
-    self.result_img = apply_kernel_to_matrix(GAUSSIAN_KERNEL_5, self.img)
+    self.smoothed_img = apply_kernel_to_matrix(GAUSSIAN_KERNEL_5, self.img)
 
 
   def __calculate_gradient_with_sobel_kernels(self):
-    self.gradient_x = apply_kernel_to_matrix(SOBEL_KERNEL_X, self.result_img)
-    self.gradient_y = apply_kernel_to_matrix(SOBEL_KERNEL_Y, self.result_img)
+    self.gradient_x = apply_kernel_to_matrix(SOBEL_KERNEL_X, self.smoothed_img)
+    self.gradient_y = apply_kernel_to_matrix(SOBEL_KERNEL_Y, self.smoothed_img)
 
   def __calculate_gradient_magnitude(self):
     powered_gradients_sum = np.power(self.gradient_x, 2) + np.power(self.gradient_y, 2)
@@ -56,8 +58,82 @@ class CannyEdgeDetector:
     self.gradient_direction = gradient_direction_in_degrees
 
 
-  def get_result_img(self):
-    if self.result_img is None:
+  def __perform_nonmaximum_suppression(self):
+    self.nonmaximum_suppressed_img = np.zeros(self.img.shape)
+
+    for row in range(self.img_height):
+      for column in range(self.img_width):
+        if not self.__does_the_magnitude_value_must_be_suppressed(row, column):
+          self.nonmaximum_suppressed_img[row, column] = self.gradient_magnitude[row, column]
+
+  def __does_the_magnitude_value_must_be_suppressed(self, row, column):    
+    is_the_extreme_horizontal_border = (column + 1 >= self.img_width) or (column - 1 <= 0)
+    is_the_extreme_vertical_border = (row + 1 >= self.img_height) or (row - 1 <= 0)
+
+    if is_the_extreme_horizontal_border or is_the_extreme_vertical_border:
+      return True
+
+    current_edge_angle = self.gradient_direction[row, column]
+    current_edge_orientation = self.__get_edge_orientation(current_edge_angle)
+    current_mag = self.gradient_magnitude[row, column]
+    mags = self.gradient_magnitude
+    
+    if current_edge_orientation == 'horizontal':
+      if current_mag < mags[row, column - 1] or current_mag < mags[row, column + 1]:
+        return True
+    elif current_edge_orientation == 'vertical':
+      if current_mag < mags[row - 1, column] or current_mag < mags[row + 1, column]:
+        return True
+    elif current_edge_orientation == '+45':
+      if current_mag < mags[row - 1, column + 1] or current_mag < mags[row + 1, column - 1]:
+        return True
+    elif current_edge_orientation == '-45':
+      if current_mag < mags[row - 1, column - 1] or current_mag < mags[row + 1, column + 1]:
+        return True
+
+    return False
+
+  def __get_edge_orientation(self, edge_angle):
+    if edge_angle >= 180:
+      return 'horizontal'
+    if edge_angle < 157.5 and edge_angle >= 112.5:
+      return '+45'
+    if edge_angle < 112.5 and edge_angle >= 67.5:
+      return 'vertical'
+    if edge_angle < 67.5 and edge_angle >= 22.5:
+      return '-45'
+    if edge_angle < 22.5 and edge_angle >= -22.5:
+      return 'horizontal'
+    if edge_angle < -22.5 and edge_angle >= -67.5:
+      return '+45'
+    if edge_angle < -67.5 and edge_angle >= -112.5:
+      return 'vertical'
+    if edge_angle < -112.5 and edge_angle >= -157.5:
+      return '-45'
+    if edge_angle < -157.5:
+      return 'horizontal'
+
+
+  def get_result_imgs(self):
+    if not self.applied:
       raise ValueError('No result image have been found. Call the apply_edge_detector method before.')
 
-    return self.result_img
+    result_imgs = CannyResultImages()
+
+    result_imgs.smoothed = self.smoothed_img
+    result_imgs.gradient_x = self.gradient_x
+    result_imgs.gradient_y = self.gradient_y
+    result_imgs.gradient_magnitude = self.gradient_magnitude
+    result_imgs.nonmaximum_suppressed = self.nonmaximum_suppressed_img
+    result_imgs.thresholded = self.thresholded_img
+
+    return result_imgs
+
+
+class CannyResultImages():
+  smoothed = None
+  gradient_x = None
+  gradient_y = None
+  gradient_magnitude = None
+  nonmaximum_suppressed = None
+  thresholded = None
--- src/main.py
+++ src/main.py
@@ -8,9 +8,14 @@ def main():
   bgr_input_img = read_input_img(input_img_name)
 
   result_img = ImageUtils.convert_bgr_img_to_grayscale(bgr_input_img)
-  result_img = apply_canny_edge_detector_to_img(result_img)
+  result_imgs = apply_canny_edge_detector_to_img(result_img)
 
-  save_output_img(result_img, input_img_name)
+  save_output_img(result_imgs.smoothed, '_output-step-1.png')
+  save_output_img(result_imgs.gradient_x, '_output-step-2.png')
+  save_output_img(result_imgs.gradient_y, '_output-step-3.png')
+  save_output_img(result_imgs.gradient_magnitude, '_output-step-4.png')
+  save_output_img(result_imgs.nonmaximum_suppressed, '_output-step-5.png')
+  save_output_img(result_imgs.thresholded, 'output-detected-edges.png')
 
 
 if __name__ == '__main__':
